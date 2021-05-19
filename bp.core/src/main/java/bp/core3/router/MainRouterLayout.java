0805c4188ccdf7687c93744fd7953dbff0ad0e67
MSI\green <greenyu0510@gmail.com>
2021-05-19 14:03:09 +0800

    initial

---
+++ bp.core/src/main/java/bp/core3/router/MainRouterLayout.java
@@ -0,0 +1,21 @@
+package bp.core3.router;
+
+import com.vaadin.flow.component.dependency.*;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.page.*;
+import com.vaadin.flow.router.*;
+import com.vaadin.flow.theme.*;
+import com.vaadin.flow.theme.lumo.*;
+
+@Theme(value = Lumo.class)
+@Route("")
+@Push
+@CssImport(value = "./css/leaflet-map-styles.css", themeFor = "leaflet-map")
+public class MainRouterLayout
+		extends
+		Div {
+
+	public MainRouterLayout() {
+		setSizeFull();
+	}
+}
